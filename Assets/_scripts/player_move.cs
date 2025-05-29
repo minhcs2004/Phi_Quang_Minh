112bf7e362d2d4282da15d623e36d445640e59e8
PhiQuangMinh <minhcs12092004@gmail.com>
2025-05-29 22:43:49 +0700

    day 1 homework

---
+++ Assets/_scripts/player_move.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class player_move : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+
+        // Di chuyển ngang
+        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+
+        // Lật hướng nhân vật
+        if (moveX > 0)
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (moveX < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        // Nhảy nếu đang chạm đất và nhấn Space
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Coi như đang đứng trên bất cứ thứ gì có Collider
+        isGrounded = true;
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+}
